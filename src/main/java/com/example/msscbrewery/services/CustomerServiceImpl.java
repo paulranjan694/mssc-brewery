33c46b93186a8f109ca85ca7be3a20a6a75dae9e
Ranjan Paul <paulranjan694@gmail.com>
2022-10-30 20:10:33 +0530

    CustomerDto(POJO), CustomerController, CustomerService and CustomerServiceImpl(Service) added

---
+++ src/main/java/com/example/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.msscbrewery.services;
+
+import com.example.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID()).customerName("Ranjan").build();
+    }
+}
